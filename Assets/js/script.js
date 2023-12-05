fea373a379c71cba18135a316a898070f96b20cc
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2023-12-05 15:38:14 -0500

    Made Local Storage save prior cities

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,50 +2,93 @@ const apiKey = "76270777d3f53b47d91433b7022b4743";
 const textData = $("#cityName");
 const formEl = $("#cityData");
 let cityName, lat, lon;
-let priorNames = [];
+let priorNames = JSON.parse(localStorage.getItem("prevCities"));
+priorNames = loadPriorNamesFn(priorNames);
+console.log("priorNames: ", priorNames)
 formEl.on("submit", function (event) {
   // console.log("click");
   event.preventDefault();
   cityName = textData.val();
-  // console.log(cityName);
-  cityData(cityName);
+  console.log(cityName);
+  if (cityName != "") {
+    cityData(cityName);
+  }
+  else {
+    console.log("empty message");
+  }
 });
 
 function cityData(name) {
   // console.log("Function activate!")
-  const apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`;
-  // console.log(apiUrlWeather);
-  fetch(apiUrlWeather)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      addBtn(data.name);
-      // console.log(data);
-      dispCurWeather(data);
-      const lat = data.coord.lat
-      const lon = data.coord.lon;
-      // console.log(lat, lon);
-      const threeHourWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
-      // console.log(threeHourWeatherUrl);
-      fetch(threeHourWeatherUrl)
+  const apiUrlGeocode = `http://api.openweathermap.org/geo/1.0/direct?q=${name}&appid=${apiKey}`;
+  //const apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`;
+
+  fetch(apiUrlGeocode).then(response => response.json()).then(function (data) {
+    console.log("Raw Data: ", data)
+    if (data.length > 0) {
+      console.log("Geocode(here): ", data[0])
+      let longName = `${data[0].name}, ${data[0].state}`;
+      console.log(`LongerName: ${data[0].name}, ${data[0].state}`);
+      addBtn(priorNames, data[0].name)
+      console.log(`Lat: ${data[0].lat}| Lon: ${data[0].lon}`);
+      //const apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${name}&units=imperial`
+      apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&lat=${data[0].lat}&lon=${data[0].lon}&units=imperial`
+      // console.log(apiUrlWeather);
+      fetch(apiUrlWeather)
         .then(function (response) {
-          // console.log("3 hour fetch")
-          // Note: dt is Unix seconds
           return response.json();
         })
         .then(function (data) {
-          // console.log(data);
-          dispFutWeather(data);
+          const cod = data.cod;
+          console.log("code: ", cod);
+          if (cod == 200) {
+            // console.log(data);
+            dispCurWeather(longName, data);
+            const lat = data.coord.lat
+            const lon = data.coord.lon;
+            console.log("Lat: ", lat, "| Lon: ", lon)
+            // 3rd Party: Don't use
+            /*
+            fetch(`https://geocode.maps.co/reverse?lat=${lat}&lon=${lon}`)
+              .then(function (response) {
+                return response.json();
+              })
+              .then(function (data) {
+                console.log("Geocode(3rd party): ", data);
+                let longCityName = `${data.address.city}, ${data.address.state}`;
+                return longCityName;
+              })
+            */
+            // console.log(lat, lon);
+            const threeHourWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+            // console.log(threeHourWeatherUrl);
+            fetch(threeHourWeatherUrl)
+              .then(function (response) {
+                // console.log("3 hour fetch")
+                // Note: dt is Unix seconds
+                return response.json();
+              })
+              .then(function (data) {
+                // console.log(data);
+                dispFutWeather(data);
+              })
+          }
+          else {
+            console.log("Invalid City Name: second catch")
+          }
         })
-    })
+    }
+    else {
+      console.log("Invalid City Name: first catch")
+    }
+  })
   //const lat, long = data.coord.lat, data.coord.lon;
 }
 
-function dispCurWeather(curData) {
-  console.log("dispCurWeather: ", curData);
+function dispCurWeather(longName, curData) {
+  //console.log("dispCurWeather: ", curData);
 
-  $("#currWeather").children().eq(0).text(`${curData.name} (${dayjs.unix(curData.dt).format('MMM D, YYYY @ h:mm a')})`);
+  $("#currWeather").children().eq(0).text(`${longName} (${dayjs.unix(curData.dt).format('MMM D, YYYY @ h:mm a')})`);
   const iconUrl = `https://openweathermap.org/img/w/${curData.weather[0].icon}.png`;
   $("#currWeather").children().eq(1).children().eq(0).attr("src", iconUrl);
   $("#currWeather").children().eq(1).children().eq(0).attr("alt", curData.weather[0].description);
@@ -57,13 +100,13 @@ function dispCurWeather(curData) {
   $("#currWeather").children().eq(2).children().eq(2).text(`Humidity: ${curData.main.humidity} %`);
 }
 function dispFutWeather(futDataArr) {
-  console.log("dispFutWeather: ", futDataArr);
+  //console.log("dispFutWeather: ", futDataArr);
   for (let i = 0; i < 5; i++) {
     let index = 8 * i + 7;
     let futData = futDataArr.list[index];
-    console.log("futData", futData)
+    //console.log("futData", futData)
     const curChild = $("#futWeather").children().eq(i);
-    console.log(curChild);
+    //console.log(curChild);
     $(curChild).children().eq(0).text(`(${dayjs.unix(futData.dt).format('M/D/YY')})`);
     const iconUrl = `https://openweathermap.org/img/w/${futData.weather[0].icon}.png`;
     $(curChild).children().eq(1).attr("src", iconUrl);
@@ -75,13 +118,14 @@ function dispFutWeather(futDataArr) {
     $(curChild).children().eq(2).children().eq(2).text(`Humidity: ${futData.main.humidity} %`);
   }
 }
-function addBtn(cityName) {
+function addBtn(priorNames, cityName) {
   console.log("Add Button:", cityName);
   if (priorNames.includes(cityName)) {
     console.log("You've already Searched for ", cityName);
   }
   else {
     priorNames.push(cityName);
+    localStorage.setItem("prevCities", JSON.stringify(priorNames));
     let btnEl = $("<button>");
     btnEl.text(cityName);
     btnEl.addClass("btn btn-secondary btn-lg btn-block my-1");
@@ -90,11 +134,10 @@ function addBtn(cityName) {
   }
 };
 
-$("#pastCities").on("click", ".btn", function(event){
-  console.log("click");
+$("#pastCities").on("click", ".btn", function (event) {
   let btnText = event.currentTarget.textContent;
-  console.log(btnText);
-  if(btnText === "Clear Cities") {
+  console.log("click: ", btnText);
+  if (btnText === "Clear Cities") {
     console.log("Clear Condition");
     clearCity();
   }
@@ -106,4 +149,21 @@ $("#pastCities").on("click", ".btn", function(event){
 
 function clearCity() {
   console.log("Clear City Function Activate!");
+  localStorage.removeItem("prevCities");
+  window.location.reload();
+}
+
+function loadPriorNamesFn(myLocalPrior) {
+  console.log("Local Prior", myLocalPrior);
+  if (myLocalPrior === null) {
+    console.log("No Local Prior")
+    return [];
+  }
+  else {
+    console.log("Local prior exists");
+    for (let i = 0; i < myLocalPrior.length; i++) {
+      addBtn([], myLocalPrior[i]);
+    }
+    return myLocalPrior;
+  }
 }
